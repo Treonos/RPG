a1ef4baf12bf8cf0ef48cfb211efdde4df19d509
Treonos <code.glasses@gmail.com>
2025-03-27 12:19:46 +0100

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    public class Enemy
+    {
+        public string Name { get; set; }
+        public string ImageSource { get; set; }
+    }
+
+    public static class GlobalEnemy
+    {
+        public static Enemy enemy1 = new Enemy();
+    }
+}
